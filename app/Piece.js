b9e502743a82c033a1f2816cf61d751045545937
edwardma33 <edwardma33@icloud.com>
2024-06-28 10:12:43 -0400

    fixed board colors and Piece path

--- app/Piece.js
+++ app/Piece.js
@@ -2,7 +2,7 @@ export default class Piece extends Image {
     constructor (isBlack, peiceType) {
         super();
 
-        this.src = `../public/${isBlack ? 'b' : 'w'}_${peiceType}.png`;
+        this.src = `./public/${isBlack ? 'b' : 'w'}_${peiceType}.png`;
         this.classList.add('piece');
     }
 }
