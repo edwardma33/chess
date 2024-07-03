97e61fe9b8388e975ff85ca630c15ca42b162947
edwardma33 <edwardma33@icloud.com>
2024-07-03 19:10:43 -0400

    Did a little work on coords for test space

--- app/test.js
+++ app/test.js
@@ -3,19 +3,25 @@ const board = document.querySelector('.board');
 const boardSize = board.clientWidth;
 
 let row = 0;
-const coordLetters
+const coordLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 
 // render board
+let currentRowNumber = 8;
+let currentRowLetter = 7;
 for (let i = 0; i<64; i++) {
     let square = document.createElement('div');
     square.style.width = `${boardSize / 8}px`;
     square.style.height = `${boardSize / 8}px`;
     square.style.color = '#f6f';
     square.classList.add('square');
+
+    
+
+    square.innerText = coordLetters[currentRowLetter] + (i + 1) % 8;
     
     // alternate colors
-    if (row % 2 ==0) {
+    if (row % 2 == 0) {
         square.classList.add(i % 2 == 0 ? 'w-square' : 'b-square')
     } else {
         square.classList.add(i % 2 == 0 ? 'b-square' : 'w-square')
@@ -24,7 +30,9 @@ for (let i = 0; i<64; i++) {
     // add square to the board
     board.appendChild(square);
 
+
     if ((i+1) % 8 == 0) {
+        currentRowLetter--;
         row++;
     }
 }
--- notes/piece_logic.md
+++ notes/piece_logic.md
@@ -1,6 +1,8 @@
 # Piece Logic
-**Each piece will need to look for**: 
-
+## Each piece will need to look for
+### squares that they CAN take a piece
+### squares that they CANT move bc teammate is in the square
+### squares that they CANT move bc they're out of bounds
 
 ## Pawn
 
